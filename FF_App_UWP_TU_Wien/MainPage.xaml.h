4b7b1a4e3d1e933bd1e2a61212e557bbfbd750dc
Philipp Mandl <philipp.mandl@tuwien.ac.at>
2020-05-25 17:58:57 +0200

    Add existing project

---
+++ FF_App_UWP_TU_Wien/MainPage.xaml.h
@@ -0,0 +1,23 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace FF_App_UWP_TU_Wien
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	private:
+		void voigasbutton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+	};
+}
