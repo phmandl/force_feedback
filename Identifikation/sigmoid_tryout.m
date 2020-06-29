aa760a69d3bd580e75ae89d8f88c44d96ee15eea
Philipp Mandl <philipp.mandl@tuwien.ac.at>
2020-06-29 22:09:53 +0200

    data files

---
+++ Identifikation/sigmoid_tryout.m
@@ -0,0 +1,18 @@
+close all
+
+center = 0;
+steepness = 1;
+scale = 2;
+
+x = -10:0.1:10;
+
+mySigmo = @(x,steepness,center) 2./(1 + exp(-steepness*(x-center)))-1;
+
+y = scale*(2*sigmf(x,[steepness center]) - 1);
+y1 = scale*mySigmo(x',steepness,center);
+
+plot(x,y,'LineWidth',2); hold on
+plot(x,y1,'LineWidth',2); hold off
+grid on
+xlabel('sigmf, P = [2 4]')
+ylim([-scale scale]*1.5)
