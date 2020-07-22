89acfa6544085ad530ceee4e19f7b4a4a86520a5
Philipp Mandl <philipp.mandl@tuwien.ac.at>
2020-07-22 14:09:25 +0200

    push

--- Matlab_Simulink/simulink_2_constant_force_effects/controller_LQR_tryout.m
+++ Matlab_Simulink/simulink_2_constant_force_effects/controller_LQR_tryout.m
@@ -2,7 +2,7 @@ clear; clc;
 close all;
 
 Var = 7.5924;
-Ts = 0.01;
+Ts = 0.02;
 
 %% Controller Script
 
@@ -20,10 +20,10 @@ R = 10;
 [K,P,e] = dlqr(sys_dis.A,sys_dis.B,Q,R);
 Kw = 1/(sys_dis.C*inv(eye(2) - sys_dis.A + sys_dis.B*K)*sys_dis.B);
 
-sys_close = ss(sys_dis.A - sys_dis.B*K, Kw*sys_dis.B,sys_dis.C,0,Ts);
+sys_close_fb = ss(sys_dis.A - sys_dis.B*K, Kw*sys_dis.B,sys_dis.C,0,Ts);
 sys_open = ss(sys_dis.A,sys_dis.B,sys_dis.C,0);
 
-% step(sys_close);
+% step(sys_close_fb);
 % damp(sys_close);
 
 %% Integration of error
@@ -31,17 +31,29 @@ Q_sys = [sys_dis.A zeros(2,1); -sys_dis.C*sys_dis.A 1];
 h = [sys_dis.B; -sys_dis.C*sys_dis.B];
 
 Q = diag([100,1,1]);
-R = 60;
+% R = 60;
+
+R = 30;
 [K_int,P,e] = dlqr(Q_sys,h,Q,R);
+K_int
+Q_close = [sys_dis.A - sys_dis.B*K_int(1:2) -K_int(end)*sys_dis.B;
+    -sys_dis.C*sys_dis.A + sys_dis.C*sys_dis.B*K_int(1:2) 1+K_int(end)*sys_dis.C*sys_dis.B];
+
+h_close = [zeros(2,1); 1];
+
+C_close = [sys_dis.C 0];
+
+sys_close_fb_int = ss(Q_close, h_close, C_close,0,Ts);
 
 % --- place
 % poles = [0.99, 0.8, 0.7];
 % K_int = place(Q_sys,h,poles)
 
 %% Build observer
-poles_obsv = real(pole(sys_close))/12;
+poles_closed = pole(sys_close_fb_int);
+poles_obsv = poles_closed(1:2)/12;
 % poles_obsv = [0;0];
-% poles_obsv = [0.5;0.5];
+poles_obsv = [0.5;0.5];
 
 P = sym('P',[2,1]);
 syms z
