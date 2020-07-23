4c69fc636a80b31eebc7d75519943832e45489e2
Philipp Mandl <philipp.mandl@tuwien.ac.at>
2020-07-23 17:00:58 +0200

    impedanz control implemented

--- Matlab_Simulink/simulink_2_constant_force_effects/controller_LQR_tryout.m
+++ Matlab_Simulink/simulink_2_constant_force_effects/controller_LQR_tryout.m
@@ -2,7 +2,7 @@ clear; clc;
 close all;
 
 Var = 7.5924;
-Ts = 0.02;
+Ts = 0.01;
 
 %% Controller Script
 
@@ -15,8 +15,8 @@ sys_con = ss(A,B,C,0);
 %% Discrete system
 sys_dis = c2d(sys_con,Ts);
 
-Q = diag([100,1]);
-R = 10;
+Q = diag([600,150]);
+R = 1;
 [K,P,e] = dlqr(sys_dis.A,sys_dis.B,Q,R);
 Kw = 1/(sys_dis.C*inv(eye(2) - sys_dis.A + sys_dis.B*K)*sys_dis.B);
 
@@ -30,15 +30,11 @@ sys_open = ss(sys_dis.A,sys_dis.B,sys_dis.C,0);
 Q_sys = [sys_dis.A zeros(2,1); -sys_dis.C*sys_dis.A 1];
 h = [sys_dis.B; -sys_dis.C*sys_dis.B];
 
-Q = diag([100,1,1]);
-% R = 60;
-
-R = 30;
+Q = diag([600,1,1]);
+R = 5;
 [K_int,P,e] = dlqr(Q_sys,h,Q,R);
-K_int
 Q_close = [sys_dis.A - sys_dis.B*K_int(1:2) -K_int(end)*sys_dis.B;
     -sys_dis.C*sys_dis.A + sys_dis.C*sys_dis.B*K_int(1:2) 1+K_int(end)*sys_dis.C*sys_dis.B];
-
 h_close = [zeros(2,1); 1];
 
 C_close = [sys_dis.C 0];
@@ -46,14 +42,14 @@ C_close = [sys_dis.C 0];
 sys_close_fb_int = ss(Q_close, h_close, C_close,0,Ts);
 
 % --- place
-% poles = [0.99, 0.8, 0.7];
+% poles = [0.96, 1, 0.52];
 % K_int = place(Q_sys,h,poles)
 
 %% Build observer
 poles_closed = pole(sys_close_fb_int);
 poles_obsv = poles_closed(1:2)/12;
 % poles_obsv = [0;0];
-poles_obsv = [0.5;0.5];
+poles_obsv = [0.4;0.4];
 
 P = sym('P',[2,1]);
 syms z
